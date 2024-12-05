c2b77128bae1e1e624fa8c9b250804d7156c90bf
Svetlozar Iliev <svetlozar321@gmail.com>
2024-12-05 20:24:01 +0200

    feat(extractor): add an extractor and generator
    Add script to generate and extract pdf from pandoc

--- generator.sh
+++ generator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pandoc --listings -H listings-setup.tex --toc -V geometry:"left=1cm, top=1cm, right=1cm, bottom=2cm" -V fontsize=12pt "$1.md" -o "$1.pdf"
-
+mkdir -p "$(pwd)/exports/$2"
+pandoc --listings -H listings-setup.tex --toc -V geometry:"left=1cm, top=1cm, right=1cm, bottom=1cm" -V fontsize=12pt "$1.md" -o "$(pwd $1)/exports/$2/$(basename $1).pdf"
