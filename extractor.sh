77645aa6846f1224a4ae84fc1fc4a6771535e40a
Svetlozar Iliev <svetlozar321@gmail.com>
2025-05-22 22:10:41 +0300

    chore(pdf): re-generate obsolete resources and misc

--- extractor.sh
+++ extractor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# sudo apt install pandoc texlive-xetex
 find cracking-coding-interview/ -type f -name '*.md' | sed -e 's/\.md$//' | xargs -I {} sh -c './generator.sh {} ccti'
 find misc-tech-topic/ -type f -name '*.md'           | sed -e 's/\.md$//' | xargs -I {} sh -c './generator.sh {} tech'
 find java-se-study/ -type f -name '*.md'             | sed -e 's/\.md$//' | xargs -I {} sh -c './generator.sh {} study'
