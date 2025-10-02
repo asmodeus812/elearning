ba94cb3ed17b159e9cc42657a19c201b91f1994d
Svetlozar Iliev <svetlozar321@gmail.com>
2025-10-02 08:09:39 +0300

    feat(tts): finalize tts generation for the rest of the resources

--- extractor.sh
+++ extractor.sh
@@ -4,14 +4,17 @@
 echo "Generating cracking-coding-interview/"
 find cracking-coding-interview/ -type f -name '*.md' | sed -e 's/\.md$//' | xargs -P 8 -I {} sh -c './generator.sh {}'
 
-echo "Generating misc-tech-topic/"
-find misc-tech-topic/ -type f -name '*.md' | sed -e 's/\.md$//' | xargs -P 8 -I {} sh -c './generator.sh {}'
-
 echo "Generating java-se-study/"
 find java-se-study/ -type f -name '*.md' | sed -e 's/\.md$//' | xargs -P 8 -I {} sh -c './generator.sh {}'
 
-echo "Converting misc-tech-topic/"
-find misc-tech-topic/ -maxdepth 1 -type f -name '*.md' | sed -e 's/\.md$//' | xargs -P 2 -I {} sh -c './converter.sh {}'
+echo "Generating misc-tech-topic/"
+find misc-tech-topic/ -type f -name '*.md' | sed -e 's/\.md$//' | xargs -P 8 -I {} sh -c './generator.sh {}'
+
+echo "Converting cracking-coding-interview/"
+find cracking-coding-interview/ -type f -name '*.md' | sed -e 's/\.md$//' | xargs -P 2 -I {} sh -c './converter.sh {}'
 
 echo "Converting java-se-study/"
 find java-se-study/ -type f -name '*.md' | sed -e 's/\.md$//' | xargs -P 2 -I {} sh -c './converter.sh {}'
+
+echo "Converting misc-tech-topic/"
+find misc-tech-topic/ -maxdepth 1 -type f -name '*.md' | sed -e 's/\.md$//' | xargs -P 2 -I {} sh -c './converter.sh {}'
